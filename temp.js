b19f04cb9a4e3bb82bf699f80c0237a334133794
gangkaishi <904304332@qq.com>
2025-02-21 06:28:51 +0800

    first temp

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hello_world
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-21
+// @description  new script
+// @author       gangkaishi
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("hello world.");
+})();
